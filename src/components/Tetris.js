181db5c21622c805a25373deb414a947657e9334
Corey Burkett <coreyburkett22@gmail.com>
2021-06-10 13:33:02 -0700

    Created tetrominos shapes and random generating function

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { createStage } from '../gameHelpers';
+
 // Components
 import Stage from './Stage';
 import Display from './Display';
@@ -8,12 +10,12 @@ import StartButton from './StartButton';
 const Tetris = () => {
     return (
         <div>
-            <Stage />
+            <Stage stage={createStage()} />
             <aside>
                 <div>
-                    <Display  text="Score"/>
-                    <Display  text="Rows"/>
-                    <Display  text="Level"/>
+                    <Display  text="Score" />
+                    <Display  text="Rows" />
+                    <Display  text="Level" />
                 </div>
                 <StartButton />
             </aside>
