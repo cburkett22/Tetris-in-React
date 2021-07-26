43273cf440a5fdfc5f40ef877d50855f76a5d69e
Corey Burkett <coreyburkett22@gmail.com>
2021-07-25 20:50:51 -0700

    Completed tetromino auto drop when game starts

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,7 @@ import { createStage, checkCollision } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // Custom Hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
@@ -32,6 +33,7 @@ const Tetris = () => {
     const startGame = () => {
         // Reset everything
         setStage(createStage());
+        setDropTime(1000);
         resetPlayer();
         setGameOver(false);
     };
@@ -50,7 +52,18 @@ const Tetris = () => {
         }
     };
 
+    const keyUp = ({ keyCode }) => {
+        console.log("Interval on");
+        if (!gameOver) {
+            if(keyCode === 40) {
+                setDropTime(1000);
+            }
+        }
+    };
+
     const dropPlayer = () => {
+        console.log("Interval off");
+        setDropTime(null);
         drop();
     };
 
@@ -68,13 +81,25 @@ const Tetris = () => {
         }
     };
 
+    useInterval(() => {
+        drop();
+    }, dropTime);
+
     return (
-        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+        <StyledTetrisWrapper
+            role="button"
+            tabIndex="0"
+            onKeyDown={e => move(e)}
+            onKeyUp={keyUp}
+        >
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
                     {gameOver ? (
-                        <Display gameOver={gameOver} text="Game Over" />
+                        <Display
+                            gameOver={gameOver}
+                            text="Game Over"
+                        />
                     ) : (
                     <div>
                         <Display  text="Score" />
