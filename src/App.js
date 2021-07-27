e8abdcb4ed1828b1a71c78f08baf5b8f825956c4
Corey Burkett <coreyburkett22@gmail.com>
2021-07-26 21:26:27 -0700

    Added background music on button click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import Tetris from './components/Tetris';
+import BackgroundMusic from './audio/tetris-audio.mp3';
+import { Howl, Howler } from 'howler';
+
+const audio = [
+  {
+    sound: BackgroundMusic,
+    label: "Play Music"
+  }
+];
+
+const playAudio = (src) => {
+  const sound = new Howl({ src });
+  sound.play();
+};
+
+const renderAudio = () => {
+  return audio.map((soundObj, index) => {
+    return (
+      <button
+        key={index}
+        onClick={(() => playAudio(soundObj.sound))}
+      >
+        {soundObj.label}
+      </button>
+    )
+  })
+};
 
 const App = () => (
   <div className="App">
-      <Tetris />
+    {renderAudio()}
+    <Tetris />
   </div>
 )
 
